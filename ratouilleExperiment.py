b3c565a99619b922a307452a849e9df81aed2f29
ratatouille-study <94420961+ratatouille-study@users.noreply.github.com>
2022-03-17 12:43:33 -0400

    added 2nd intro question

--- ratouilleExperiment.py
+++ ratouilleExperiment.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.2.3),
-    on March 02, 2022, at 13:48
+    on March 10, 2022, at 10:40
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -51,7 +51,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\ACLab\\Desktop\\Ratatouille Study\\ratouilleExperiment.py',
+    originPath='C:\\Experiment\\ratouilleExperiment.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
@@ -65,7 +65,7 @@ frameTolerance = 0.001  # how close to onset before 'same' frame
 
 # Setup the Window
 win = visual.Window(
-    size=[1920, 1080], fullscr=True, screen=0, 
+    size=[1820, 980], fullscr=False, screen=0, 
     winType='pyglet', allowGUI=False, allowStencil=False,
     monitor='testMonitor', color=(-1.0000, -1.0000, -1.0000), colorSpace='rgb',
     blendMode='avg', useFBO=True, 
@@ -97,13 +97,24 @@ key_resp = keyboard.Keyboard()
 # Initialize components for Routine "Intro2"
 Intro2Clock = core.Clock()
 text_2 = visual.TextStim(win=win, name='text_2',
-    text='Rating the foods is time sensitive, so please be attentive to the screen. There will be a 15 second break every so often. \n\nTo answer the question, please use the mouse to click on the slider.\n\n\nBefore we practice the task, we have 1 more question to ask you. Press [Space] to proceed.',
+    text='Rating the foods is time sensitive, so please be attentive to the screen. There will be a 15 second break every so often. \n\nTo answer the question, please use the mouse to click on the slider.\n\n\nBefore we practice the task, we have 2 more questions to ask you. Press [Space] to proceed.',
     font='Open Sans',
     pos=(0, 0), height=0.035, wrapWidth=None, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
     languageStyle='LTR',
     depth=0.0);
 key_resp_2 = keyboard.Keyboard()
+
+# Initialize components for Routine "pasteating"
+pasteatingClock = core.Clock()
+text_8 = visual.TextStim(win=win, name='text_8',
+    text="Have you eaten or drank anything other than water in the last 3 hours?\n\n\nPress 'y' for Yes and 'n' for No.",
+    font='Open Sans',
+    pos=(0, 0), height=0.035, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp_7 = keyboard.Keyboard()
 
 # Initialize components for Routine "hungerscale"
 hungerscaleClock = core.Clock()
@@ -274,7 +285,7 @@ key_resp_6 = keyboard.Keyboard()
 text_7 = visual.TextStim(win=win, name='text_7',
     text='Press [Space] if you saw the image',
     font='Open Sans',
-    pos=(0, -0.35), height=0.065, wrapWidth=None, ori=0.0, 
+    pos=(0, -0.35), height=0.04, wrapWidth=None, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
     languageStyle='LTR',
     depth=-2.0);
@@ -301,7 +312,7 @@ endClock = core.Clock()
 text_6 = visual.TextStim(win=win, name='text_6',
     text='Thank you for participating in this experiment! You will receive your SONA credits within 48 hours.\n\nPress [Space] to exit.',
     font='Open Sans',
-    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
+    pos=(0, 0), height=0.04, wrapWidth=None, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
     languageStyle='LTR',
     depth=0.0);
@@ -492,6 +503,102 @@ thisExp.addData('key_resp_2.started', key_resp_2.tStartRefresh)
 thisExp.addData('key_resp_2.stopped', key_resp_2.tStopRefresh)
 thisExp.nextEntry()
 # the Routine "Intro2" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# ------Prepare to start Routine "pasteating"-------
+continueRoutine = True
+# update component parameters for each repeat
+key_resp_7.keys = []
+key_resp_7.rt = []
+_key_resp_7_allKeys = []
+# keep track of which components have finished
+pasteatingComponents = [text_8, key_resp_7]
+for thisComponent in pasteatingComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+pasteatingClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "pasteating"-------
+while continueRoutine:
+    # get current time
+    t = pasteatingClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=pasteatingClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text_8* updates
+    if text_8.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text_8.frameNStart = frameN  # exact frame index
+        text_8.tStart = t  # local t and not account for scr refresh
+        text_8.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text_8, 'tStartRefresh')  # time at next scr refresh
+        text_8.setAutoDraw(True)
+    
+    # *key_resp_7* updates
+    waitOnFlip = False
+    if key_resp_7.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        key_resp_7.frameNStart = frameN  # exact frame index
+        key_resp_7.tStart = t  # local t and not account for scr refresh
+        key_resp_7.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(key_resp_7, 'tStartRefresh')  # time at next scr refresh
+        key_resp_7.status = STARTED
+        # keyboard checking is just starting
+        waitOnFlip = True
+        win.callOnFlip(key_resp_7.clock.reset)  # t=0 on next screen flip
+        win.callOnFlip(key_resp_7.clearEvents, eventType='keyboard')  # clear events on next screen flip
+    if key_resp_7.status == STARTED and not waitOnFlip:
+        theseKeys = key_resp_7.getKeys(keyList=['y', 'n', 'Y', 'N'], waitRelease=False)
+        _key_resp_7_allKeys.extend(theseKeys)
+        if len(_key_resp_7_allKeys):
+            key_resp_7.keys = _key_resp_7_allKeys[-1].name  # just the last key pressed
+            key_resp_7.rt = _key_resp_7_allKeys[-1].rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in pasteatingComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "pasteating"-------
+for thisComponent in pasteatingComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('text_8.started', text_8.tStartRefresh)
+thisExp.addData('text_8.stopped', text_8.tStopRefresh)
+# check responses
+if key_resp_7.keys in ['', [], None]:  # No response was made
+    key_resp_7.keys = None
+thisExp.addData('key_resp_7.keys',key_resp_7.keys)
+if key_resp_7.keys != None:  # we had a response
+    thisExp.addData('key_resp_7.rt', key_resp_7.rt)
+thisExp.addData('key_resp_7.started', key_resp_7.tStartRefresh)
+thisExp.addData('key_resp_7.stopped', key_resp_7.tStopRefresh)
+thisExp.nextEntry()
+# the Routine "pasteating" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
 # ------Prepare to start Routine "hungerscale"-------
@@ -1001,7 +1108,7 @@ routineTimer.reset()
 # set up handler to look after randomisation of conditions etc
 trials = data.TrialHandler(nReps=1.0, method='random', 
     extraInfo=expInfo, originPath=-1,
-    trialList=data.importConditions('C:\\Users\\ACLab\\Desktop\\Ratatouille Study\\imageList.xlsx', selection='0:222'),
+    trialList=data.importConditions('C:/Experiment/imageList.xlsx', selection='0:222'),
     seed=None, name='trials')
 thisExp.addLoop(trials)  # add the loop to the experiment
 thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
@@ -1023,7 +1130,6 @@ for thisTrial in trials:
     if trials.thisTrialN % 20 == 0 and trials.thisTrialN > 0:
         continueRoutine = True
     routineTimer.add(15.000000)
-    
     # update component parameters for each repeat
     # keep track of which components have finished
     CrosshairComponents = [text]
@@ -1287,10 +1393,10 @@ for thisTrial in trials:
     continueRoutine = False
     if trials.thisTrialN % 73 == 0 and trials.thisTrialN > 0:
         continueRoutine = True
-    routineTimer.add(2.000000)
+    routineTimer.add(2.500000)
     
     #Set Image in random order
-    imgs = ['C:\\Users\\ACLab\\Desktop\\Ratatouille Study\\images\\Attention\\beets.jpg','C:\\Users\\ACLab\\Desktop\\Ratatouille Study\\images\\Attention\\carrots.jpg','C:\\Users\\ACLab\\Desktop\\Ratatouille Study\\images\\Attention\\cauliflower.jpg']
+    imgs = ['C://Experiment//images//Attention//beets.jpg','C://Experiment//images//Attention//carrots.jpg','C://Experiment//images//Attention//cauliflower.jpg']
     # update component parameters for each repeat
     image_2.setImage(imgs[count])
     key_resp_6.keys = []
@@ -1330,7 +1436,7 @@ for thisTrial in trials:
             image_2.setAutoDraw(True)
         if image_2.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > image_2.tStartRefresh + 2-frameTolerance:
+            if tThisFlipGlobal > image_2.tStartRefresh + 2.5-frameTolerance:
                 # keep track of stop time/frame for later
                 image_2.tStop = t  # not accounting for scr refresh
                 image_2.frameNStop = frameN  # exact frame index
@@ -1352,7 +1458,7 @@ for thisTrial in trials:
             win.callOnFlip(key_resp_6.clearEvents, eventType='keyboard')  # clear events on next screen flip
         if key_resp_6.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > key_resp_6.tStartRefresh + 2-frameTolerance:
+            if tThisFlipGlobal > key_resp_6.tStartRefresh + 2.5-frameTolerance:
                 # keep track of stop time/frame for later
                 key_resp_6.tStop = t  # not accounting for scr refresh
                 key_resp_6.frameNStop = frameN  # exact frame index
@@ -1377,7 +1483,7 @@ for thisTrial in trials:
             text_7.setAutoDraw(True)
         if text_7.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > text_7.tStartRefresh + 2-frameTolerance:
+            if tThisFlipGlobal > text_7.tStartRefresh + 2.5-frameTolerance:
                 # keep track of stop time/frame for later
                 text_7.tStop = t  # not accounting for scr refresh
                 text_7.frameNStop = frameN  # exact frame index
@@ -1400,6 +1506,7 @@ for thisTrial in trials:
         # refresh the screen
         if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
             win.flip()
+    
     # -------Ending Routine "attention"-------
     if count >= 3:
         count = 0
@@ -1428,6 +1535,16 @@ for thisTrial in trials:
 
 # ------Prepare to start Routine "hungerscale"-------
 continueRoutine = True
+
+#slider has no tick marks
+for i, line in enumerate(slider.tickLines.sizes):
+    if i not in [0,9]:  # Where 0 and 10 are your first and last tick locations
+        slider.tickLines.sizes[i][1] = 0
+slider.tickLines._needVertexUpdate = True
+
+#adjust slider red marker
+slider.marker.size = (.03, .03)
+
 # update component parameters for each repeat
 slider.reset()
 # keep track of which components have finished
